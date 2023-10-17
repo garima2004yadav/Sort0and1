c40d281841e78951c1aaa76cf4f42357c296d15e
garima2004yadav <115277218+garima2004yadav@users.noreply.github.com>
2023-10-17 22:33:38 +0530

    Add files via upload

---
+++ Sort0and1.java
@@ -0,0 +1,21 @@
+public class Sort0and1 {
+    public static void main(String[] args) {
+        int arr[] = {0,1,0,1,0,1,0,0,1,1};
+        int zeroCount = 0;
+        for(int i=0;i<arr.length;i++){
+            if (arr[i] == 0)
+            zeroCount +=1;
+    }
+    for(int i=0;i<arr.length;i++){
+        if(i<zeroCount){
+            arr[i]=0;
+        } else{
+            arr[i]=1;
+        }
+    }
+    for(int i : arr){
+        System.out.println(i +" ");
+    }
+    
+}
+}
